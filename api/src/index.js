5f77befc3d2adf0295015f0bc63bdbe119b60ef6
Yuriy Sobchak <yuriysobchak@benders-Mac-mini-3.local>
2021-12-07 18:17:30 +0200

    docker configs

--- api/src/index.js
+++ api/src/index.js
@@ -1,10 +1,6 @@
 const express = require('express');
-const {connectDb} = require('./helpers/db');
 const {port} = require('./config');
 const app = express();
-
-const db = connectDb();
-console.log(db);
 
 app.get('/', (req, res) => {
 	res.send("Our api server is working");
